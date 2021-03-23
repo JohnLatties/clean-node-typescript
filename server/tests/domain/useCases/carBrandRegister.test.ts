561f47fd8291f5fc03c509d2af789c7ab0137b65
Jonathan Latties <johnlatties@gmail.com>
2021-03-23 11:49:33 -0300

    test: update RegisterCarBrandRegisterScraping to user CarBrand domain model

--- server/tests/domain/useCases/carBrandRegister.test.ts
+++ server/tests/domain/useCases/carBrandRegister.test.ts
@@ -3,6 +3,8 @@ import { CarBrandScrapingModel } from '../../../src/domain/useCases/registerCarB
 import RegisterCarBrandRegisterScraping, { IRegisterCarBrandRegisterScraping } from '../../../src/domain/useCases/registerCarBrandScraping'
 import InMemoryIarBrandRepository from '../../../src/infra/data/inMemory/InMemoryCarBrandRepository'
 import { ICarBrandRepository } from '../../../src/domain/models/carBrand/ICarBranRepository'
+import { InvalidCarBrandError } from '../../../src/domain/share/errors/InvalidCarBrandError'
+import { fail } from '../../../src/crosscutting/either'
 
 type SutTypes = {
   sut: IRegisterCarBrandRegisterScraping
@@ -10,7 +12,55 @@ type SutTypes = {
   carBrandRepository: ICarBrandRepository
 }
 
-const carBrandScraping: CarBrandScrapingModel = {
+const carBrandScrapingWithoutTitle: CarBrandScrapingModel = {
+  title: '',
+  detailsUrl: 'url',
+  image: 'image.png',
+  cars: [{
+    name: 'MINI Clubman',
+    detailsUrl: 'url',
+    image: 'image.png',
+    price: 20000
+  }]
+}
+
+const carBrandScrapingWithoutImage: CarBrandScrapingModel = {
+  title: 'Mini',
+  detailsUrl: 'url',
+  image: '',
+  cars: [{
+    name: 'MINI Clubman',
+    detailsUrl: 'url',
+    image: 'image.png',
+    price: 20000
+  }]
+}
+
+const carBrandScrapingWithCarWithoutName: CarBrandScrapingModel = {
+  title: 'Mini',
+  detailsUrl: 'url',
+  image: 'image.png',
+  cars: [{
+    name: '',
+    detailsUrl: 'url',
+    image: 'image.png',
+    price: 20000
+  }]
+}
+
+const carBrandScrapingWithCarWithoutImage: CarBrandScrapingModel = {
+  title: 'Mini',
+  detailsUrl: 'url',
+  image: 'image.png',
+  cars: [{
+    name: 'MINI Clubman',
+    detailsUrl: 'url',
+    image: '',
+    price: 20000
+  }]
+}
+
+const carBrandScrapingSuccess: CarBrandScrapingModel = {
   title: 'Mini',
   detailsUrl: 'url',
   image: 'image.png',
@@ -22,14 +72,14 @@ const carBrandScraping: CarBrandScrapingModel = {
   }]
 }
 
-const MakeCarBrandScrapingData = (): ICarBrandScrapingData => ({
+const MakeCarBrandScrapingData = (testCarBrandScrapingModel: CarBrandScrapingModel): ICarBrandScrapingData => ({
   searchData: function (): Promise<CarBrandScrapingModel[]> {
-    return new Promise((resolve) => resolve([carBrandScraping]))
+    return new Promise((resolve) => resolve([testCarBrandScrapingModel]))
   }
 })
 
-const makeSut = (): SutTypes => {
-  const carBrandScrapingData: ICarBrandScrapingData = MakeCarBrandScrapingData()
+const makeSut = (testCarBrandScrapingModel: CarBrandScrapingModel): SutTypes => {
+  const carBrandScrapingData: ICarBrandScrapingData = MakeCarBrandScrapingData(testCarBrandScrapingModel)
   const carBrandRepository: ICarBrandRepository = InMemoryIarBrandRepository()
   const sut = RegisterCarBrandRegisterScraping(carBrandScrapingData, carBrandRepository)
 
@@ -37,35 +87,74 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Car Brand Register use case', () => {
+  test('Should not create carBrands with invalid name', async () => {
+    const { sut } = makeSut(carBrandScrapingWithoutTitle)
+
+    const carBrandOrError = await sut.execute()
+    expect(carBrandOrError)
+      .toEqual(
+        fail(new InvalidCarBrandError(`The name "${carBrandScrapingWithoutTitle.title}" is invalid.`))
+      )
+  })
+
+  test('Should not create carBrands with invalid image', async () => {
+    const { sut } = makeSut(carBrandScrapingWithoutImage)
+
+    const carBrandOrError = await sut.execute()
+    expect(carBrandOrError)
+      .toEqual(
+        fail(new InvalidCarBrandError(`The image "${carBrandScrapingWithoutTitle.title}" is invalid.`))
+      )
+  })
+
+  test('Should not create carBrands with cars without name', async () => {
+    const { sut } = makeSut(carBrandScrapingWithCarWithoutName)
+
+    const carBrandOrError = await sut.execute()
+    expect(carBrandOrError)
+      .toEqual(
+        fail(new InvalidCarBrandError('The car position "0" is invalid.'))
+      )
+  })
+
+  test('Should not create carBrands with cars without image', async () => {
+    const { sut } = makeSut(carBrandScrapingWithCarWithoutImage)
+
+    const carBrandOrError = await sut.execute()
+    expect(carBrandOrError)
+      .toEqual(
+        fail(new InvalidCarBrandError('The car position "0" is invalid.'))
+      )
+  })
+
   test('Should call CarBrandScrapingQuery and run "execute" function ', async () => {
-    const { sut, carBrandScrapingData } = makeSut()
+    const { sut, carBrandScrapingData } = makeSut(carBrandScrapingSuccess)
     const carBrandScrapingQuerySpy = jest.spyOn(carBrandScrapingData, 'searchData')
     await sut.execute()
     expect(carBrandScrapingQuerySpy).toBeCalled()
   })
 
   test('Should call CarBrandRepository and run "addMany" function ', async () => {
-    const { sut, carBrandRepository } = makeSut()
+    const { sut, carBrandRepository } = makeSut(carBrandScrapingSuccess)
     const carBrandRepositorySpy = jest.spyOn(carBrandRepository, 'addMany')
     await sut.execute()
     expect(carBrandRepositorySpy).toBeCalled()
   })
 
   test('Should be no car brand before receiving data form scraping', async () => {
-    const { carBrandRepository } = makeSut()
+    const { carBrandRepository } = makeSut(carBrandScrapingSuccess)
     const emptyCarBrandList = await carBrandRepository.findAll()
 
     expect(emptyCarBrandList.length).toBe(0)
   })
 
   test('Should add new car brands receiving data form scraping', async () => {
-    const { sut, carBrandRepository } = makeSut()
+    const { sut, carBrandRepository } = makeSut(carBrandScrapingSuccess)
     const emptyCarBrandList = await carBrandRepository.findAll()
     expect(emptyCarBrandList.length).toBe(0)
 
     await sut.execute()
     const carbrandList = await carBrandRepository.findAll()
-
     expect(carbrandList.length > 0).toBe(true)
   })
 })
