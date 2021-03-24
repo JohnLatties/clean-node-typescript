2e4c696e8e1c631258ec84e5c37e6a33b25b4ba4
Jonathan Latties <johnlatties@gmail.com>
2021-03-24 09:27:19 -0300

    fix: set correct router

--- server/src/main/api/routes/carBrand.route.ts
+++ server/src/main/api/routes/carBrand.route.ts
@@ -3,5 +3,5 @@ import { adaptRoute } from '../../adapters/routesAdapter'
 import { createCarBrandController } from '../../factories/carBrand'
 
 export default (router: Router): void => {
-  router.get('/carbrand', adaptRoute(createCarBrandController()))
+  router.get('/carbrands', adaptRoute(createCarBrandController()))
 }
