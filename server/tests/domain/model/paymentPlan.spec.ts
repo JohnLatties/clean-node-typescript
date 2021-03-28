fb4d174a2f85d6b038e5d7234856bb8d75bdf611
Jonathan Latties <johnlatties@gmail.com>
2021-03-28 01:22:34 -0300

    test: implmentes Refinancing domail model

---
+++ server/tests/domain/model/paymentPlan.spec.ts
@@ -0,0 +1,33 @@
+import { PaymentPlan } from '../../../src/domain/models/refinancingProposal/PaymentPlan'
+import { InvalidPaymentPlan } from '../../../src/domain/share/errors/InvalidPaymentPlan'
+import { fail } from '../../../src/crosscutting/either'
+
+describe('PaymentPlan', () => {
+  test('Should not create PaymentPlan with invalida value', () => {
+    const value = -550
+    const months = 36
+
+    const paymentPlanOrError = PaymentPlan.create({ value, months })
+
+    expect(paymentPlanOrError).toEqual(fail(new InvalidPaymentPlan(value, months)))
+  })
+
+  test('Should not create PaymentPlan with invalida months', () => {
+    const value = 340
+    const months = 0
+
+    const paymentPlanOrError = PaymentPlan.create({ value, months })
+
+    expect(paymentPlanOrError).toEqual(fail(new InvalidPaymentPlan(value, months)))
+  })
+
+  test('Should create paymentPlan when all correct data', () => {
+    const value = 340
+    const months = 36
+
+    const paymentPlanOrError = PaymentPlan.create({ value, months })
+    const paymentPlan = paymentPlanOrError.value as PaymentPlan
+    expect(paymentPlan.months).toBe(months)
+    expect(paymentPlan.valeu).toBe(value)
+  })
+})
---
+++ server/tests/domain/model/refinancingProposal.spec.ts
@@ -0,0 +1,88 @@
+import { RefinancingProposal } from '../../../src/domain/models/refinancingProposal/RefinancingProposal'
+import { InvalidCarBrandError } from '../../../src/domain/share/errors/InvalidCarBrandError'
+import { InvalidPaymentPlan } from '../../../src/domain/share/errors/InvalidPaymentPlan'
+import { InvalidAPRError } from '../../../src/domain/share/errors/InvalidAPRError'
+import { InvalidCarError } from '../../../src/domain/share/errors/InvalidCarError'
+import { fail } from '../../../src/crosscutting/either'
+
+const InvalidCarBrandProposal = {
+  key: '',
+  name: '',
+  image: ''
+}
+
+const InvalidCarProposal = {
+  key: '',
+  name: '',
+  image: '',
+  price: 0
+}
+
+const InvalidPriceCarProposal = {
+  key: 'fecfe64c-307c-4d20-9e57-7ebce9b8fe1d',
+  name: 'Acura ILX 2019',
+  image: 'image.png',
+  price: 0
+}
+
+const CarBrandProposal = {
+  key: 'key-car-brandacura',
+  name: 'Acura',
+  image: 'image.png'
+}
+
+const CarProposal = {
+  key: 'fecfe64c-307c-4d20-9e57-7ebce9b8fe1d',
+  name: 'Acura ILX 2019',
+  image: 'image.png',
+  price: 23416
+}
+
+describe('RefinancingProposal', () => {
+  test('Should not create RefinancingProposal with invalida carBrand', () => {
+    const apr = 10
+    const refinancingProposalOrError = RefinancingProposal.create(InvalidCarBrandProposal, CarProposal, apr)
+    expect(refinancingProposalOrError).toEqual(fail(new InvalidCarBrandError('Missing car brand')))
+  })
+
+  test('Should not create RefinancingProposal with invalida car', () => {
+    const apr = 10
+    const refinancingProposalOrError = RefinancingProposal.create(CarBrandProposal, InvalidCarProposal, apr)
+    expect(refinancingProposalOrError).toEqual(fail(new InvalidCarError(0)))
+  })
+
+  test('Should not create RefinancingProposal with invalida APR', () => {
+    const apr = 0
+    const refinancingProposalOrError = RefinancingProposal.create(CarBrandProposal, CarProposal, apr)
+    expect(refinancingProposalOrError).toEqual(fail(new InvalidAPRError(apr)))
+  })
+
+  test('Should not create RefinancingProposal with invalida payment plan', () => {
+    const apr = 10
+    const refinancingProposalOrError = RefinancingProposal.create(CarBrandProposal, InvalidPriceCarProposal, apr)
+    const resultError = refinancingProposalOrError.value as InvalidPaymentPlan
+    expect(resultError.message).toMatch(/The payment, with value: 0 and months: 36 is invalid./)
+    expect(resultError).toBeInstanceOf(InvalidPaymentPlan)
+  })
+
+  test('Should create RefinancingProposal when all correct data', () => {
+    const apr = 10
+    const refinancingProposalOrError = RefinancingProposal.create(CarBrandProposal, CarProposal, apr)
+    const resutl = refinancingProposalOrError.value as RefinancingProposal
+
+    expect(resutl.proposalNumber).not.toBe(null)
+    expect(resutl.carBrand.key).toBe(CarBrandProposal.key)
+    expect(resutl.carBrand.name).toBe(CarBrandProposal.name)
+    expect(resutl.car.key).toBe(CarProposal.key)
+    expect(resutl.car.name).toBe(CarProposal.name)
+    expect(resutl.apr).toBe(apr)
+    expect(resutl.paymentOptions.length).toBe(2)
+    expect(resutl.paymentOptions)
+      .toEqual(expect
+        .arrayContaining([
+          expect.objectContaining({ valeu: 845.58, months: 36 }),
+          expect.objectContaining({ valeu: 682.97, months: 48 })
+        ])
+      )
+  })
+})
